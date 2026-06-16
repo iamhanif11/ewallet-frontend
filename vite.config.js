f102ab7ff17ce0e7c877ee14f8db19911cf27bff
M. Hanif Irfan <mhirfan173@gmail.com>
2026-06-16 19:30:59 +0700

    setup: vite config

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    proxy:{
+      'api': {
+        target: 'http://172.18.135.102:8080', 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
